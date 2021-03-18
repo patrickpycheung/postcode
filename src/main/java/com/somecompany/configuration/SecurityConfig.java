62c1e6fbf7957e04367e73bae866d447b9d8bb14
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-18 22:07:10 +1100

    Configure HTTP access.

---
+++ src/main/java/com/somecompany/configuration/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.somecompany.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+			.and()
+				.authorizeRequests()
+					.antMatchers("/", "/error", "/webjars/**", "/actuator/**")
+						.permitAll()
+					.anyRequest()
+						.authenticated()
+			.and()
+				.oauth2ResourceServer()
+					.jwt();
+	}
+}
