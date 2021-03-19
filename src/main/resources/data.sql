4d4ec00ea9b88072d99fa206bed20b2c2028e2b4
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-19 11:46:31 +1100

    Amend sample DB data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 INSERT INTO POSTCODE (postcode, suburb, suburbinfo) VALUES
   (3000, 'MELBOURNE, VIC', 'Melbourne Info'),
+  (3121, 'BURNLEY, VIC', 'Burnley Info'),
   (3121, 'RICHMOND, VIC', 'Richmond Info'),
-  (3053, 'CARLTON, VIC', 'Carlton Info');
+  (3053, 'CARLTON, VIC', 'Carlton Info'),
+  (2124, 'PARRAMATTA, NSW', 'Parramatta Info 2124'),
+  (2150, 'PARRAMATTA, NSW', 'Parramatta Info 2150');
