46c6d5753d24f29d52d47f3bc5d069e3e8541dbd
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-19 15:16:17 +1100

    Amend entity class Postcode, add "id" column.

--- src/main/java/com/somecompany/model/Postcode.java
+++ src/main/java/com/somecompany/model/Postcode.java
@@ -1,8 +1,11 @@
 package com.somecompany.model;
 
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Getter;
@@ -13,8 +16,19 @@ import lombok.Setter;
 @Setter
 public class Postcode {
 
-	@EmbeddedId
-	private PostcodePK postcodePK;
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "postcode")
+	@NotEmpty(message = "Postcode cannot be null nor empty!")
+	@Pattern(regexp = "^[0-9]+$", message = "Postcode must be a number!")
+	private String postcode;
+
+	@Column(name = "suburb")
+	@NotEmpty(message = "Suburb cannot be null nor empty!")
+	@Size(min = 1, max = 100, message = "Suburb must have length between 1 and 100!")
+	private String suburb;
 
 	@Column(name = "suburbinfo")
 	@Size(min = 0, max = 100, message = "Suburb information must have length between 0 and 100!")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO POSTCODE (postcode, suburb, suburbinfo) VALUES
-  (3000, 'MELBOURNE, VIC', 'Melbourne Info'),
-  (3121, 'BURNLEY, VIC', 'Burnley Info'),
-  (3121, 'RICHMOND, VIC', 'Richmond Info'),
-  (3053, 'CARLTON, VIC', 'Carlton Info'),
-  (2124, 'PARRAMATTA, NSW', 'Parramatta Info 2124'),
-  (2150, 'PARRAMATTA, NSW', 'Parramatta Info 2150');
+INSERT INTO POSTCODE (id, postcode, suburb, suburbinfo) VALUES
+  (1,3000, 'MELBOURNE, VIC', 'Melbourne Info'),
+  (2,3121, 'BURNLEY, VIC', 'Burnley Info'),
+  (3,3121, 'RICHMOND, VIC', 'Richmond Info'),
+  (4,3053, 'CARLTON, VIC', 'Carlton Info'),
+  (5,2124, 'PARRAMATTA, NSW', 'Parramatta Info 2124'),
+  (6,2150, 'PARRAMATTA, NSW', 'Parramatta Info 2150');
