b434a80214042505c4d341a9672d5db1a8834a1e
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-19 12:00:17 +1100

    Amend application name.

--- src/main/java/com/somecompany/PostcodeApplication.java
+++ src/main/java/com/somecompany/PostcodeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuburbAndPostcodeApplication {
+public class PostcodeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SuburbAndPostcodeApplication.class, args);
+		SpringApplication.run(PostcodeApplication.class, args);
 	}
 
 }
